d12c62cf3aa10ee343457aaf809809d76f20811e
xtofl <kristoffel.pirard@gmail.com>
2016-06-14 20:27:13 +0200

    [impl] Common + Client/Server proxies

---
+++ src/main/java/xtofl/CommonProxy.java
@@ -0,0 +1,18 @@
+package xtofl;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+public class CommonProxy {
+
+    public void preInit(FMLInitializationEvent e) {
+
+    }
+
+    public void init(FMLInitializationEvent e) {
+
+    }
+
+    public void postInit(FMLInitializationEvent e) {
+
+    }
+}
---
+++ src/main/java/xtofl/ServerProxy.java
@@ -0,0 +1,22 @@
+package xtofl;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+public class ServerProxy extends CommonProxy {
+
+    @Override
+    public void preInit(FMLInitializationEvent e) {
+        super.preInit(e);
+    }
+
+    @Override
+    public void init(FMLInitializationEvent e) {
+        super.init(e);
+    }
+
+    @Override
+    public void postInit(FMLInitializationEvent e) {
+        super.postInit(e);
+    }
+
+}
